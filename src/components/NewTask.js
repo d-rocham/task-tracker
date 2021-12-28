c5332f1897fdf25487273691215de22980129cc2
d-rocham <d_rocham@outlook.com>
2021-12-28 16:26:07 -0500

    Added small gap between form items

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -5,7 +5,7 @@ const NewTask = () => {
     }
 
     return (
-        <form action="" className="create-new-task inline-flex justify-around flex-wrap">
+        <form action="" className="create-new-task inline-flex justify-around flex-wrap gap-2">
             <input type="text" name="" id="" placeholder="What's on your mind?" className="p-1 rounded-md bg-gray-100 border-transparent" />
             <input type="datetime-local" name="" id="" className="p-1 rounded-md bg-gray-100 border-transparent" />
             <input type="submit" value="Add!" onClick={onClick} className="py-1 px-2 rounded-md bg-emerald-600 border-transparent text-white font-bold cursor-pointer" />
