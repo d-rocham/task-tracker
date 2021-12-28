6a98270375ffdb5257228ca0f1548af727227c5e
d-rocham <d_rocham@outlook.com>
2021-12-28 16:21:45 -0500

    Installed @tailwindcss/forms plugin, created basic form styling

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -5,9 +5,10 @@ const NewTask = () => {
     }
 
     return (
-        <form action="" className="create-new-task text-center inline">
-            <input type="text" name="" id="" placeholder="What's on your mind?" />
-            <input type="submit" value="Add!" onClick={onClick} />
+        <form action="" className="create-new-task inline-flex justify-around flex-wrap">
+            <input type="text" name="" id="" placeholder="What's on your mind?" className="p-1 rounded-md bg-gray-100 border-transparent" />
+            <input type="datetime-local" name="" id="" className="p-1 rounded-md bg-gray-100 border-transparent" />
+            <input type="submit" value="Add!" onClick={onClick} className="py-1 px-2 rounded-md bg-emerald-600 border-transparent text-white font-bold cursor-pointer" />
         </form>
     )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { IoClose, IoCheckmarkSharp } from "react-icons/io5"
 
 const Task = ({ task, onDelete, onChangeStatus }) => {
     return (
-        <div className="card-container flex flex-row bg-gray-200 mb-2 pr-2">
+        <div className="card-container flex flex-row bg-gray-200 mb-2 pr-2 shadow-md">
             <div className={`side-decorator ${task.completed ? "bg-green-600" : "bg-sky-600"} w-2 mr-2`}></div>
             <div className="info-container flex flex-col grow my-1">
                 <h3 className="task-title text-xl">{task.content}</h3>
